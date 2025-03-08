af6d73ad8fdbfd6e797aeb8041035db8cb6ef8d8
Noel Vaca <noel.vm.2501@gmail.com>
2025-03-08 10:55:22 -0400

    feat: Fundamentos de progrmación

---
+++ Demo/DemoConsola/Program.cs
@@ -0,0 +1,111 @@
+﻿// Esto es un comentario
+/* Esto es un
+ * un comentario en múltiples líneas
+ */
+
+// Variables
+var variable = "Cualquier valor";
+bool esActivo = false;
+int entero = 20;
+long enteroLargo = 7545454545455;
+double real = 3.1416;
+decimal real2 = 3.1416m;
+float real3 = 3.1416f;
+char caracter = 'A';
+string cadena = "Esto es una cadena";
+Int16 enteroCorto = 3;
+Int32 entero2 = 20;
+Int64 enteroLargo2 = 7545454545455;
+DateTime fecha = DateTime.Now;
+
+// Constantes
+const double PI = 3.1416;
+const string SIGLA_MATERIA = "SIS457";
+
+// Operadores y expresiones
+entero = 15;
+entero2 += 7;
+entero2 %= 2;
+bool esPar = entero2 % 2 == 0;
+int a = 3 + 6 * 4;
+int b = (3 + 6) * 4;
+
+// Conversiones y cadenas
+string conversionCadena = Convert.ToString(real);
+int conversionEntero = Convert.ToInt32("20");
+bool conversionBooleano = Convert.ToBoolean("true");
+
+string materia = "Fundamentos de la Programación";
+string concatenar = materia + " - " + SIGLA_MATERIA;
+string concatenar2 = $"{materia} - {SIGLA_MATERIA}";
+int longitudCadena = materia.Length;
+string mayusculas = concatenar.ToUpper();
+string minusculas = concatenar.ToLower();
+string invertir = new string(materia.Reverse().ToArray());
+string quitarEspaciosExtremos = materia.Trim();
+string reemplazo = materia.Replace(" ", "");
+string subcadena = materia.Substring(0, 5);
+string[] separar = materia.Split(" ");
+
+// Estructuras de control
+bool esMayorCero;
+if (cadena.Length > 0) esMayorCero = true; 
+else esMayorCero = false;
+esMayorCero = cadena.Length > 0 ? true : false;
+esMayorCero = cadena.Length > 0;
+
+switch (materia) 
+{
+    case "Informática": materia += " - SIS457"; break;
+    default: materia = materia.ToUpper(); break;
+}
+
+int contador = 0;
+while (contador < 3)
+{
+    Console.WriteLine($"Contador while: {contador}");
+    contador++;
+}
+
+do
+{
+    Console.WriteLine($"Contador do-while: {contador}");
+    contador--;
+} while (contador > 0);
+
+for (int i = 0; i < separar.Length; i++)
+{
+    Console.WriteLine($"Ciclo for -> Posición {i}: {separar[i]}");
+}
+
+foreach (string item in separar)
+{
+    Console.WriteLine($"Ciclo foreach: {item}");
+}
+
+// Métodos
+void saludo(string nombre)
+{
+    Console.WriteLine($"Hola {nombre}");
+}
+saludo("Juan");
+
+string revertirCadena(string cadena)
+{
+    return new string(cadena.Reverse().ToArray());
+}
+Console.WriteLine(revertirCadena("HOLA"));
+
+int suma(int a, int b)
+{
+    return a + b;
+}
+
+// Factoral
+Console.WriteLine("Ingrese un número para calcular el factorial: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+int factorial = 1;
+for (int i = 2; i <= Math.Abs(numero); i++) factorial *= i;
+if (numero < 0) factorial *= -1;
+Console.WriteLine($"El factorial de {numero} es {factorial}");
+

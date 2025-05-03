b58c1edd110186994865cfe5525aa977d73ec39d
Noel Vaca <noel.vm.2501@gmail.com>
2025-05-03 10:57:51 -0400

    feat: Proyecto WebMinerva, configuración BD, CRUD inicial Productos

---
+++ Minerva/WebMinerva/Models/Producto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebMinerva.Models;
+
+public partial class Producto
+{
+    public int Id { get; set; }
+
+    public string Codigo { get; set; } = null!;
+
+    public string Descripcion { get; set; } = null!;
+
+    public string UnidadMedida { get; set; } = null!;
+
+    public decimal Saldo { get; set; }
+
+    public decimal PrecioVenta { get; set; }
+
+    public string UsuarioRegistro { get; set; } = null!;
+
+    public DateTime FechaRegistro { get; set; }
+
+    public short Estado { get; set; }
+
+    public virtual ICollection<CompraDetalle> CompraDetalles { get; set; } = new List<CompraDetalle>();
+}
